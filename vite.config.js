242f8118e6bf8e021865c7408b22d39ec4880bed
Louange <benithaiyuyisenga2002@gmail.com>
2024-10-18 18:40:10 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,31 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import resolve from '@rollup/plugin-node-resolve';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     resolve(),
+//   ],
+//   base: '/',
+//   server: {
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@components': path.resolve(__dirname, 'src/components'),
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['@chakra-ui/react'],
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import resolve from '@rollup/plugin-node-resolve';
@@ -21,7 +49,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['@chakra-ui/react'],
+      external: [], // Leave this array empty or remove it entirely
     },
   },
 });
